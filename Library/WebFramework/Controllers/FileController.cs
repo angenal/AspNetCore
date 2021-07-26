57d279717fb04d413b98893b20eb56ff1bf36ff1
yangzhou <angenal@hotmail.com>
2021-07-26 11:33:36 +0800

    update

--- Library/WebFramework/Controllers/FileController.cs
+++ Library/WebFramework/Controllers/FileController.cs
@@ -6,15 +6,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
-using UploadStream;
 using WebCore;
 
 namespace WebFramework.Controllers
 {
     /// <summary>
-    /// 文件系统
+    /// 文件
     /// </summary>
     [ApiController]
+    [DisableFormModelBinding]
     [Route("api/[controller]/[action]")]
     public class FileController : ApiController
     {
@@ -33,13 +33,15 @@ namespace WebFramework.Controllers
         /// <summary>
         /// 上传
         /// </summary>
-        [HttpPost("upload")]
+        [HttpPost]
         [Produces("application/json")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> Upload()
         {
             var section = config.GetSection("Upload:WebRootPath");
             if (!section.Exists()) return Error("未配置该功能");
+            if (!Request.HasFormContentType) return Error("未上传任何文件");
 
             var root = section.Value.Trim('/');
             var path = Path.Combine(env.WebRootPath, root);
@@ -47,22 +49,15 @@ namespace WebFramework.Controllers
             var url = Request.RootPath() + root;
             var files = new List<object>();
 
+            // upload file
             //var buffer = new byte[4096];
-            //var files = new List<IFormFile>();
-            await this.StreamFiles(async file =>
+            var result = await Request.UploadFile(async file =>
             {
-                if (file.Length > 1)
-                {
-                    var filename = (Guid.NewGuid().ToString("N") + Path.GetExtension(file.FileName)).ToLower();
-                    var filepath = Path.Combine(path, filename);
-                    using (var stream = System.IO.File.Create(filepath)) await file.CopyToAsync(stream);
-                    files.Add(new { file.Name, file.FileName, file.ContentDisposition, file.ContentType, file.Length, Path = $"{url}/{filename}" });
-                }
-                //using (var stream = file.OpenReadStream())
-                //{
-                //    while (await stream.ReadAsync(buffer.AsMemory(0, buffer.Length)) > 0) { }
-                //}
-                //files.Add(file);
+                var filename = (Guid.NewGuid().ToString("N") + Path.GetExtension(file.FileName)).ToLower();
+                var filepath = Path.Combine(path, filename);
+                using (var stream = System.IO.File.Create(filepath)) await file.CopyToAsync(stream);
+                files.Add(new { key = file.Name, value = file.FileName, file.ContentType, file.Length, Path = $"{url}/{filename}" });
+                //using (var stream = file.OpenReadStream()) while (await stream.ReadAsync(buffer.AsMemory(0, buffer.Length)) > 0) { }
             });
 
             // ModelState is still validated from model
---
+++ Library/WebFramework/UploadFile.cs
@@ -0,0 +1,178 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebFramework
+{
+    public static class HttpRequestExtensions
+    {
+        public static async Task<FormValueProvider> UploadFile(this HttpRequest request, Func<IFormFile, Task> func)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(request.ContentType))
+                throw new Exception($"Expected a multipart request, but got {request.ContentType}");
+
+            // Used to accumulate all the form url encoded key value pairs in the request.
+            var formOptions = new FormOptions();
+            var formAccumulator = new KeyValueAccumulator();
+            var boundary = MultipartRequestHelper.GetBoundary(MediaTypeHeaderValue.Parse(request.ContentType), formOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, request.Body);
+
+            // section may have already been read (if for example model binding is not disabled), please use [DisableFormModelBinding]
+            var section = await reader.ReadNextSectionAsync();
+
+            while (section != null)
+            {
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out var contentDispositionHeader);
+
+                if (hasContentDispositionHeader)
+                {
+                    if (contentDispositionHeader.IsFileDisposition())
+                    {
+                        FileMultipartSection fileSection = section.AsFileSection();
+
+                        // process file stream
+                        await func(new MultipartFile(fileSection.FileStream, fileSection.Name, fileSection.FileName)
+                        {
+                            ContentType = fileSection.Section.ContentType,
+                            ContentDisposition = fileSection.Section.ContentDisposition
+                        });
+                    }
+                    else if (contentDispositionHeader.IsFormDisposition())
+                    {
+                        // Content-Disposition: form-data; name="key"
+                        // Do not limit the key name length here because the multipart headers length limit is already in effect.
+                        var key = HeaderUtilities.RemoveQuotes(contentDispositionHeader.Name);
+                        var encoding = section.GetEncoding();
+                        if (encoding == null)
+                            throw new NullReferenceException($"Null encoding");
+
+                        using var streamReader = new StreamReader(section.Body, encoding, detectEncodingFromByteOrderMarks: true, bufferSize: 1024, leaveOpen: true);
+                        // The value length limit is enforced by MultipartBodyLengthLimit
+                        var value = await streamReader.ReadToEndAsync();
+                        if (string.Equals(value, "undefined", StringComparison.OrdinalIgnoreCase))
+                            value = string.Empty;
+
+                        formAccumulator.Append(key.Value, value);
+
+                        if (formAccumulator.ValueCount > formOptions.ValueCountLimit)
+                            throw new InvalidDataException($"Form key count limit {formOptions.ValueCountLimit} exceeded.");
+                    }
+                }
+
+                // Drains any remaining section body that has not been consumed and reads the headers for the next section.
+                //section = request.Body.CanSeek && request.Body.Position == request.Body.Length ? null : await reader.ReadNextSectionAsync();
+                section = await reader.ReadNextSectionAsync();
+            }
+
+            // Bind form data to a model
+            return new FormValueProvider(BindingSource.Form, new FormCollection(formAccumulator.GetResults()), CultureInfo.CurrentCulture);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class DisableFormModelBindingAttribute : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            var formValueProviderFactory = context.ValueProviderFactories.OfType<FormValueProviderFactory>().FirstOrDefault();
+            if (formValueProviderFactory != null) context.ValueProviderFactories.Remove(formValueProviderFactory);
+
+            var formFileValueProviderFactory = context.ValueProviderFactories.OfType<FormFileValueProviderFactory>().FirstOrDefault();
+            if (formFileValueProviderFactory != null) context.ValueProviderFactories.Remove(formFileValueProviderFactory);
+
+            var jqueryFormValueProviderFactory = context.ValueProviderFactories.OfType<JQueryFormValueProviderFactory>().FirstOrDefault();
+            if (jqueryFormValueProviderFactory != null) context.ValueProviderFactories.Remove(jqueryFormValueProviderFactory);
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context) { }
+    }
+
+    public class MultipartFile : IFormFile
+    {
+        // Stream.CopyTo method uses 128KB as the default buffer size.
+        const int DefaultBufferSize = 128 * 1024;
+        readonly Stream Stream;
+
+        public string ContentType
+        {
+            get { return Headers["Content-Type"]; }
+            set { Headers["Content-Type"] = value; }
+        }
+
+        public string ContentDisposition
+        {
+            get { return Headers["Content-Disposition"]; }
+            set { Headers["Content-Disposition"] = value; }
+        }
+
+        public IHeaderDictionary Headers { get; set; } = new HeaderDictionary();
+        public long Length => Stream.Length;
+        public string Name { get; set; }
+        public string FileName { get; set; }
+
+        public MultipartFile(Stream stream, string name, string filename)
+        {
+            Stream = stream;
+            Name = name;
+            FileName = filename;
+        }
+
+        public void CopyTo(Stream target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            Stream.CopyTo(target);
+        }
+
+        public Task CopyToAsync(Stream target, CancellationToken cancellationToken = default)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            return Stream.CopyToAsync(target, DefaultBufferSize, cancellationToken);
+        }
+
+        public Stream OpenReadStream()
+        {
+            return Stream;
+        }
+    }
+
+    public static class MultipartRequestHelper
+    {
+        // Content-Type: multipart/form-data; boundary="----WebKitFormBoundarymx2fSWqWSd0OxQqq"
+        // The spec says 70 characters is a reasonable limit.
+        public static string GetBoundary(MediaTypeHeaderValue contentType, int lengthLimit)
+        {
+            var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary).Value;
+            if (string.IsNullOrWhiteSpace(boundary))
+                throw new InvalidDataException("Missing content-type boundary.");
+
+            if (boundary.Length > lengthLimit)
+                throw new InvalidDataException($"Multipart boundary length limit {lengthLimit} exceeded.");
+
+            return boundary;
+        }
+
+        public static bool IsMultipartContentType(string contentType)
+        {
+            return !string.IsNullOrEmpty(contentType) && contentType.IndexOf("multipart/", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+    }
+
+    public static class MultipartSectionExtensions
+    {
+        public static Encoding GetEncoding(this MultipartSection section)
+        {
+            var hasMediaTypeHeader = MediaTypeHeaderValue.TryParse(section.ContentType, out MediaTypeHeaderValue mediaType);
+            return !hasMediaTypeHeader ? Encoding.UTF8 : mediaType.Encoding;
+        }
+    }
+}
