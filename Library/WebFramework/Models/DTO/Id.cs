593d252c92fe0e308012da079e4f20a54f2fd08a
yangzhou <angenal@hotmail.com>
2021-08-09 10:51:57 +0800

    update

--- Library/WebFramework/Models/DTO/Id.cs
+++ Library/WebFramework/Models/DTO/Id.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebFramework.Models.DTO
 {
+    /// <summary>
+    ///
+    /// </summary>
+    public class IdInput
+    {
+        /// <summary>
+        /// 编号(可选)
+        /// </summary>
+        [Display(Name = "编号")]
+        public string Id { get; set; }
+    }
     /// <summary>
     ///
     /// </summary>
@@ -13,5 +25,17 @@ namespace WebFramework.Models.DTO
         [Display(Name = "编号")]
         [Required(ErrorMessage = "{0} 为必填项")]
         public string Id { get; set; }
+    }
+    /// <summary>
+    ///
+    /// </summary>
+    public class IdListInputDto
+    {
+        /// <summary>
+        /// 编号列表
+        /// </summary>
+        [Display(Name = "编号")]
+        [Required(ErrorMessage = "{0} 为必填项")]
+        public List<string> Id { get; set; }
     }
 }
