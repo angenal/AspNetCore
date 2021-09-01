e8119b3004ef15426d745127d2e8efa619acde11
yangzhou <angenal@hotmail.com>
2021-09-01 10:57:30 +0800

    update

--- Library/WebFramework/Services/SwaggerModule.cs
+++ Library/WebFramework/Services/SwaggerModule.cs
@@ -51,8 +51,10 @@ namespace WebFramework.Services
                 // add operation filter which sets default values.
                 if (ApiVersionService.UseVersionedApiExplorer) c.OperationFilter<SwaggerDefaultValues>();
                 // integrate xml comments, set project properties to generate XML file.
-                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetEntryAssembly().GetName().Name}.xml"), true);
-                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"), true);
+                if (File.Exists($"{Assembly.GetEntryAssembly().GetName().Name}.xml"))
+                    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetEntryAssembly().GetName().Name}.xml"), true);
+                if (File.Exists($"{Assembly.GetExecutingAssembly().GetName().Name}.xml"))
+                    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"), true);
                 // add authentication security scheme.
                 string name = "Authorization", scheme = JwtBearerDefaults.AuthenticationScheme;
                 var securityScheme = new OpenApiSecurityScheme
