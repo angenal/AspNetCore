702c4dfbe3895de8f4ef94693827cc4fb026e732
yangzhou <angenal@hotmail.com>
2021-09-14 17:30:13 +0800

    update

--- Microservice/NATS.Services/Program.cs
+++ Microservice/NATS.Services/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using System;
@@ -10,42 +11,17 @@ using System.Reflection;
 
 namespace NATS.Services
 {
+    /// <summary></summary>
     public class Program
     {
+        /// <summary></summary>
         public static void Main(string[] args)
         {
             // Init
             WebCore.Main.Init();
 
-            // Create default logger, reference https://github.com/serilog/serilog-settings-configuration/blob/dev/sample/Sample
-            string basePath = Directory.GetCurrentDirectory(), serilogAppsettings = "appsettings.serilog.json";
-            var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
-            if (File.Exists(Path.Join(basePath, serilogAppsettings)))
-            {
-                var configuration = new ConfigurationBuilder().SetBasePath(basePath)
-                    .AddJsonFile(serilogAppsettings, optional: false, reloadOnChange: true)
-                    .Build();
-                Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration)
-                    .CreateLogger();
-            }
-            else
-            {
-                var builder = new ConfigurationBuilder().SetBasePath(basePath)
-                    .AddEnvironmentVariables("DOTNET_")
-                    .AddCommandLine(args)
-                    .AddJsonFile("appsettings.json")
-                    .AddJsonFile($"appsettings.{environmentName}.json", true, true);
-
-                if ("Development".Equals(environmentName, StringComparison.OrdinalIgnoreCase))
-                    builder.AddUserSecrets(Assembly.GetExecutingAssembly());
-
-                var configuration = builder.Build();
-                Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration)
-                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                    .Enrich.FromLogContext()
-                    .WriteTo.Console()
-                    .CreateBootstrapLogger();
-            }
+            // Create logger
+            ConfigureLogging(args);
 
             // Performance mark [start]
             var watch = Stopwatch.StartNew();
@@ -73,14 +49,58 @@ namespace NATS.Services
             }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        // Create logger, reference https://github.com/serilog/serilog-settings-configuration/blob/dev/sample/Sample
+        /// <summary></summary>
+        static void ConfigureLogging(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+            string basePath = Directory.GetCurrentDirectory(), serilogAppsettings = "appsettings.serilog.json";
+            if (File.Exists(Path.Join(basePath, serilogAppsettings)))
+            {
+                var builder = new ConfigurationBuilder().SetBasePath(basePath)
+                    .AddJsonFile(serilogAppsettings, optional: false, reloadOnChange: true);
+
+                if ("Development".Equals(environmentName, StringComparison.OrdinalIgnoreCase))
+                    builder.AddUserSecrets(Assembly.GetExecutingAssembly());
+
+                var configuration = builder.Build();
+                Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+            }
+            else
+            {
+                var builder = new ConfigurationBuilder().SetBasePath(basePath)
+                    .AddEnvironmentVariables("DOTNET_")
+                    .AddCommandLine(args)
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{environmentName}.json", true, true);
+
+                if ("Development".Equals(environmentName, StringComparison.OrdinalIgnoreCase))
+                    builder.AddUserSecrets(Assembly.GetExecutingAssembly());
+
+                var configuration = builder.Build();
+                Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration)
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateBootstrapLogger();
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary></summary>
+        static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddHostedService<Worker>();
+        }
+
+        /// <summary></summary>
+        static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                // Sets the host lifetime to WindowsServiceLifetime
-                .UseWindowsService(options => options.ServiceName = typeof(Program).Namespace)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                })
+                .ConfigureLogging(builder => builder.ClearProviders())
+                .ConfigureServices(ConfigureServices) // Use this method to add services to the container.
+                .UseWindowsService(options => options.ServiceName = typeof(Program).Namespace) // Sets the host lifetime to WindowsServiceLifetime.
                 .UseSerilog();
     }
 }
