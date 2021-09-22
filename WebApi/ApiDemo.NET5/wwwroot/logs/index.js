9705f7f9e5b4c09af6f7aec037d2ba23d2c0807f
yangzhou <angenal@hotmail.com>
2021-09-22 20:45:25 +0800

    update

--- WebApi/ApiDemo.NET5/wwwroot/logs/index.js
+++ WebApi/ApiDemo.NET5/wwwroot/logs/index.js
@@ -18,13 +18,13 @@ function log0Load() {
                 info.html('记录为空').show();
                 return;
             }
-            info.html('已加载' + (20 * (p > 1 ? p - 1 : 0) + data.rows.length) + '条记录（共' + data.records + '条）').show();
+            info.html('已加载<span id="log00">' + (20 * (p > 1 ? p - 1 : 0) + data.rows.length) + '</span>条记录（共<span id="log01">' + data.records + '</span>条）').show();
             if (data.total > p) btn.attr('page', (p + 1)).show();
             if (searchText) tbl.html('');
             for (var i = 0; i < data.rows.length; i++) {
                 var item = data.rows[i];
-                var tpl = '<tr><td><span class="text-muted bg-info" style="cursor:pointer" onclick="$(\'#log0' + (item.id.replace('-', '')) + '\').toggle()">' + (item.path) + '</span><br><span class="text-info bg-warning">' + (item.trace) + '</span></td><td class="text-info vmiddle">' + (item.request) + '</td><td class="text-muted vmiddle">' + (item.time) + '</td><td class="vmiddle"><button type="button" class="btn btn-xs btn-danger" page="1" onclick="log0Delete(this,\'' + (item.id) + '\')">删除</button></td></tr>';
-                tpl += '<tr id="log1' + (item.id.replace('-', '')) + '" style="display:none"><td colspan="4" align="left"><pre>' + (item.response) + '</pre></td></tr>';
+                var tpl = '<tr><td><span class="text-muted bg-info" style="cursor:pointer" onclick="$(\'#log0' + (item.id.replace('-', '')) + '\').toggle()">' + (item.path) + '</span><br><span class="text-info bg-warning">' + (item.trace) + '</span></td><td class="text-info vmiddle"><pre>' + (item.request) + '</pre></td><td class="text-muted vmiddle">' + (item.time) + '</td><td class="vmiddle"><button type="button" class="btn btn-xs btn-danger" page="1" onclick="log0Delete(this,\'' + (item.id) + '\')">删除</button></td></tr>';
+                tpl += '<tr id="log0' + (item.id.replace('-', '')) + '" style="display:none"><td colspan="4" align="left"><pre>' + (item.response) + '</pre></td></tr>';
                 //console.log(tpl);
                 tbl.append($(tpl));
             }
@@ -51,7 +51,7 @@ function log1Load() {
                 info.html('记录为空').show();
                 return;
             }
-            info.html('已加载' + (20 * (p > 1 ? p - 1 : 0) + data.rows.length) + '条记录（共' + data.records + '条）').show();
+            info.html('已加载<span id="log10">' + (20 * (p > 1 ? p - 1 : 0) + data.rows.length) + '</span>条记录（共<span id="log11">' + data.records + '</span>条）').show();
             if (data.total > p) btn.attr('page', (p + 1)).show();
             if (searchText) tbl.html('');
             for (var i = 0; i < data.rows.length; i++) {
@@ -85,6 +85,8 @@ function log0Delete(btn, id) {
         success: (data) => {
             //console.log(data);
             tr.remove(); tr1.remove();
+            $('#log00').text((parseInt($('#log00').text()) - 1));
+            $('#log01').text((parseInt($('#log01').text()) - 1));
         },
         error: (errMsg) => {
             console.log(errMsg);
@@ -101,6 +103,8 @@ function log1Delete(btn, id) {
         success: (data) => {
             //console.log(data);
             tr.remove(); tr1.remove();
+            $('#log10').text((parseInt($('#log10').text()) - 1));
+            $('#log11').text((parseInt($('#log11').text()) - 1));
         },
         error: (errMsg) => {
             console.log(errMsg);
