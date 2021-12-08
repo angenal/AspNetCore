9003b2b9576f7f3deb652d1531a2f8415ae53676
yangzhou <angenal@hotmail.com>
2021-12-08 19:45:24 +0800

    update

--- Library/WebSwagger/Resources/translate.js
+++ Library/WebSwagger/Resources/translate.js
@@ -1,121 +1,107 @@
-﻿/**
- * 初始化翻译组件
- */
-function initialTranslation(obj) {
-    obj.html(resource_globalization["SelectDefinition"]);
-    $.initialize('section.models.is-open', function () {
-        $(this).find('h4 > span').html(resource_globalization["Schemas"]);
-    });
-}
+$(document).ready(function () {
+    var res = resource_globalization;
 
-/**
- * 全部翻译
- */
-function allTranslation() {
+    $.initialize('.swagger-ui .topbar .download-url-wrapper', function () {
+        $(this).find('.select-label span').html(res["SelectDefinition"]);
+    });
+
+    $.initialize('section.models.is-open', function () {
+        $(this).find('h4 > span').html(res["Schemas"]);
+    });
+
     $.initialize('h4.opblock-title:not(.parameter__name)', function () {
-        $(this).find('span').html(resource_globalization["Description"]);
+        $(this).find('span').html(res["Description"]);
     });
 
     $.initialize('h4.opblock-title.parameter__name', function () {
-        $(this).html(resource_globalization["RequestBody"]);
+        $(this).html(res["RequestBody"]);
     });
 
     $.initialize('table.parameters', function () {
 
-        $(this).find('th.parameters-col_name').html(resource_globalization["Name"]);
-        $(this).find('th.parameters-col_description').html(resource_globalization["Description"]);
+        $(this).find('th.parameters-col_name').html(res["Name"]);
+        $(this).find('th.parameters-col_description').html(res["Description"]);
     });
 
     $.initialize('button.try-out__btn', function () {
-        $(this).html(resource_globalization["TryItOut"]);
+        $(this).html(res["TryItOut"]);
     });
 
     $.initialize('button.try-out__btn.cancel', function () {
-        $(this).html(resource_globalization["Cancel"]);
+        $(this).html(res["Cancel"]);
     });
 
     $.initialize('button.execute', function () {
-        $(this).html(resource_globalization["Execute"]);
+        $(this).html(res["Execute"]);
     });
 
     $.initialize('button.btn-clear', function () {
-        $(this).html(resource_globalization["Clear"]);
+        $(this).html(res["Clear"]);
     });
 
     $.initialize('button.authorize ', function () {
-        $(this).find('span').html(resource_globalization["Authorize"]);
+        $(this).find('span').html(res["Authorize"]);
     });
 
     $.initialize('button.authorize.modal-btn.auth', function () {
-        $(this).html(resource_globalization["Authorize"]);
-
-        $(this).closest('.modal-ux-inner').find('.modal-ux-header > h3').html(resource_globalization["AvailableAuth"]);
-
+        $(this).html(res["Authorize"]);
+        $(this).closest('.modal-ux-inner').find('.modal-ux-header > h3').html(res["AvailableAuth"]);
     });
 
     $.initialize('.btn-done.modal-btn.auth', function () {
-        $(this).html(resource_globalization["Close"]);
+        $(this).html(res["Close"]);
     });
 
     $.initialize('.parameter__enum ', function () {
-        $(this).find('i').html(resource_globalization["AvailableValues"]);
+        $(this).find('i').html(res["AvailableValues"]);
     });
 
     $.initialize('a.tablinks[data-name="example"]', function () {
-        $(this).html(resource_globalization["ExampleValue"]);
+        $(this).html(res["ExampleValue"]);
     });
 
     $.initialize('a.tablinks[data-name="model"]', function () {
-        $(this).html(resource_globalization["Schema"]);
+        $(this).html(res["Schema"]);
     });
 
     $.initialize('.responses-wrapper', function () {
-        $(this).find('.opblock-section-header > h4').html(resource_globalization["Responses"]);
+        $(this).find('.opblock-section-header > h4').html(res["Responses"]);
     });
 
     $.initialize('.request-url', function () {
-        $(this).prev('h4').html(resource_globalization["RequestUrl"]);
+        $(this).prev('h4').html(res["RequestUrl"]);
     });
 
     $.initialize('table.responses-table.live-responses-table', function () {
-        $(this).prev('h4').html(resource_globalization["ServerResponse"]);
-        $(this).find('tr.responses-header > td.response-col_status').html(resource_globalization["Code"]);
-        $(this).find('tr.responses-header > td.response-col_description').html(resource_globalization["Details"]);
+        $(this).prev('h4').html(res["ServerResponse"]);
+        $(this).find('tr.responses-header > td.response-col_status').html(res["Code"]);
+        $(this).find('tr.responses-header > td.response-col_description').html(res["Details"]);
 
-        $(this).find('tr.response > td.response-col_description').find('h5:first').html(resource_globalization["ResponseBody"]);
-        $(this).find('tr.response > td.response-col_description').find('h5').eq(1).html(resource_globalization["ResponseHeaders"]);
+        $(this).find('tr.response > td.response-col_description').find('h5:first').html(res["ResponseBody"]);
+        $(this).find('tr.response > td.response-col_description').find('h5').eq(1).html(res["ResponseHeaders"]);
 
-        $(this).parent('div').next('h4').html(resource_globalization["Responses"]);
-
-
+        $(this).parent('div').next('h4').html(res["Responses"]);
     });
 
     $.initialize('table.responses-table:not(.live-responses-table)', function () {
-        $(this).find('tr.responses-header > td.response-col_status').html(resource_globalization["Code"]);
-        $(this).find('tr.responses-header > td.response-col_description').html(resource_globalization["Description"]);
-
+        $(this).find('tr.responses-header > td.response-col_status').html(res["Code"]);
+        $(this).find('tr.responses-header > td.response-col_description').html(res["Description"]);
     });
 
     $.initialize('.response-control-media-type__title', function () {
-        $(this).html(resource_globalization["MediaType"]);
+        $(this).html(res["MediaType"]);
     });
 
     $.initialize('.download-contents', function () {
-        $(this).html(resource_globalization["Download"]);
+        $(this).html(res["Download"]);
     });
 
     $.initialize('.col_header.response-col_links', function () {
-        $(this).html(resource_globalization["Links"]);
+        $(this).html(res["Links"]);
     });
 
     $.initialize('.response-col_links:not(.col_header)', function () {
-        $(this).find('i').html(resource_globalization["NoLinks"]);
+        $(this).find('i').html(res["NoLinks"]);
     });
 
-}
-
-$(document).ready(function () {
-    var lbl = $(this);
-    initialTranslation(lbl.find('span'));
-    allTranslation();
 });
