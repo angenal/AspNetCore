1b46147fe62761bcf395dd2280e399114507bcd3
Angenal <angenal@hotmail.com>
2022-03-04 14:38:07 +0800

    Update natsql.bat

--- Microservice/NATS.Services/natsql.bat
+++ Microservice/NATS.Services/natsql.bat
@@ -1,9 +1,9 @@
 :: Windows Service Manager: https://nssm.cc/download  https://nssm.cc/commands
-:: nssm install natsql-test C:/wax/nats/subscribe/natsql.exe -i 5 -c test.yaml
-:: nssm set natsql-test AppDirectory C:/wax/nats/subscribe
+:: nssm install natsql-test C:/nats/services/NATS.Services.exe -i 5 -c test.yaml
+:: nssm set natsql-test AppDirectory C:/nats/services
 :: nssm start natsql-test
 
-natsql -i 5 -c natsql.yaml
+NATS.Services.exe -i 5 -c natsql.yaml
 
-cd C:\Users\Administrator\source\repos\ConsoleApp\ConsoleApp\bin
-ConsoleApp.exe 4 1000000 natsql.test-001
+cd C:/nats/services
+NATS.Services.exe 1 natsql.test-001
